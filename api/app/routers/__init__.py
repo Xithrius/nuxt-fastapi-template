a259d5e3b9419c30bdabb873416066a94f043cf5
Xithrius <xithrius@gmail.com>
2024-04-27 16:43:43 -0700

    FastAPI-users addition to API

--- api/app/routers/__init__.py
+++ api/app/routers/__init__.py
@@ -2,8 +2,10 @@ from fastapi.routing import APIRouter
 
 from .echo import router as echo_router
 from .health import router as health_router
+from .users import router as users_router
 
 api_router = APIRouter(prefix="/api")
 
 api_router.include_router(echo_router, tags=["Echo"])
 api_router.include_router(health_router, tags=["Monitoring"])
+api_router.include_router(users_router)
